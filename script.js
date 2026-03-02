b522ffdf4747ef33a6a7f164876710214bb71979
Rea-Phalatse <2613960@students.wits.ac.za>
2026-03-02 16:27:07 +0200

    Searching by full text.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function searchCountry(countryName) {
 
         // Fetch country data
         const response = await fetch(
-            `https://restcountries.com/v3.1/name/${countryName}`
+            `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
         );
 
         if (!response.ok) {
